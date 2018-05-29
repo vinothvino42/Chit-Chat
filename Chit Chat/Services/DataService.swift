8b15f54607c172ccdb03a12bb23b2e5a6fc2ee60
vinoth <vinothvino42@gmail.com>
2018-05-29 23:49:49 +0530

    LoginVC and Data Service added

---
+++ Chit Chat/Services/DataService.swift
@@ -0,0 +1,41 @@
+//
+//  DataService.swift
+//  Chit Chat
+//
+//  Created by Vinoth Vino on 29/05/18.
+//  Copyright © 2018 CoderEarth. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    static let instance = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERS = DB_BASE.child("users")
+    private var _REF_GROUPS = DB_BASE.child("groups")
+    private var _REF_FEED = DB_BASE.child("feed")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    
+    var REF_USERS: DatabaseReference {
+        return _REF_USERS
+    }
+    
+    var REF_GROUPS: DatabaseReference {
+        return _REF_GROUPS
+    }
+    
+    var REF_FEED: DatabaseReference {
+        return _REF_FEED
+    }
+    
+    func createDBUser(uid: String, userData: Dictionary<String, Any>) {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+}
