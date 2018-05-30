9dc1c88aca7ca34935556a58fa6da76c7e242715
vinoth <vinothvino42@gmail.com>
2018-05-30 15:56:07 +0530

    Design UI of Profile VC

---
+++ Chit Chat/Controller/ProfileVC/ProfileVC.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileVC.swift
+//  Chit Chat
+//
+//  Created by Vinoth Vino on 30/05/18.
+//  Copyright © 2018 CoderEarth. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func signOutDidTap(_ sender: Any) {
+        
+    }
+    
+}
