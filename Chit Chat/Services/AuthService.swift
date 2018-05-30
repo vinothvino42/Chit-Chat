8b3856e5dffc7a347db1e6b664257a499b43f82a
vinoth <vinothvino42@gmail.com>
2018-05-30 12:55:12 +0530

    Firebase Authentication added

---
+++ Chit Chat/Services/AuthService.swift
@@ -0,0 +1,37 @@
+//
+//  AuthService.swift
+//  Chit Chat
+//
+//  Created by Vinoth Vino on 30/05/18.
+//  Copyright © 2018 CoderEarth. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, withPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.providerID, "email": email]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    func loginUser(withEmail email: String, withPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                loginComplete(false, error)
+                return
+            }
+            loginComplete(true, nil)
+        }
+    }
+}
