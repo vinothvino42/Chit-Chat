6a2796ee5d830f2e6cf1d5306aa0768da38e30c0
vinoth <vinothvino42@gmail.com>
2018-05-31 18:10:13 +0530

    Getting Feed messages and Message Model added

---
+++ Chit Chat/Model/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Chit Chat
+//
+//  Created by Vinoth Vino on 31/05/18.
+//  Copyright © 2018 CoderEarth. All rights reserved.
+//
+
+import Foundation
---
+++ Chit Chat/View/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  Chit Chat
+//
+//  Created by Vinoth Vino on 31/05/18.
+//  Copyright © 2018 CoderEarth. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
