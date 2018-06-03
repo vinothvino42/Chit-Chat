10d7dac7a15aa22d870fe99df83a745bd7f1bdfb
vinoth <vinothvino42@gmail.com>
2018-06-03 14:46:09 +0530

    Upload group members, title and description

--- Chit Chat/Controller/GroupVC/CreateGroupsVC/CreateGroupVC.swift
+++ Chit Chat/Controller/GroupVC/CreateGroupsVC/CreateGroupVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateGroupVC: UIViewController {
 
@@ -46,7 +47,19 @@ class CreateGroupVC: UIViewController {
     }
 
     @IBAction func doneDidTap(_ sender: Any) {
-        
+        if titleTextField.text != "" && descriptionTextField.text != "" {
+            DataService.instance.getIds(forUsername: chosenUserArray, handler: { (idsArray) in
+                var userIds = idsArray
+                userIds.append((Auth.auth().currentUser?.uid)!)
+                DataService.instance.createGroups(withTitle: self.titleTextField.text!, andDescription: self.descriptionTextField.text!, forUserIds: userIds, handler: { (groupCreated) in
+                    if groupCreated {
+                        self.dismiss(animated: true, completion: nil)
+                    } else {
+                        print("Failed to create groups. Please try again later")
+                    }
+                })
+            })
+        }
     }
     
     @IBAction func closeDidTap(_ sender: Any) {
--- Chit Chat/Services/DataService.swift
+++ Chit Chat/Services/DataService.swift
@@ -86,4 +86,23 @@ class DataService {
             handler(emailArray)
         }
     }
+    
+    func getIds(forUsername usernames: [String], handler: @escaping (_ uidArray: [String]) -> ()) {
+        REF_USERS.observeSingleEvent(of: .value) { (userSnapshot) in
+            var idArray = [String]()
+            guard let userSnapshot = userSnapshot.children.allObjects as? [DataSnapshot] else { return }
+            for user in userSnapshot {
+                let email = user.childSnapshot(forPath: "email").value as! String
+                if usernames.contains(email) {
+                    idArray.append(user.key)
+                }
+            }
+            handler(idArray)
+        }
+    }
+    
+    func createGroups(withTitle title: String, andDescription description: String, forUserIds ids: [String], handler: @escaping (_ groupCreated: Bool) -> ()) {
+        REF_GROUPS.childByAutoId().updateChildValues(["title" : title, "description": description, "members": ids])
+        handler(true)
+    }
 }
