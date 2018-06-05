17437c512e03ffedc313fe5136365c241adef0d6
vinoth <vinothvino42@gmail.com>
2018-06-05 13:03:35 +0530

    Add Google Authentication and Tab Bar Controller

--- Chit Chat/Controller/AuthVC/AuthVC.swift
+++ Chit Chat/Controller/AuthVC/AuthVC.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 import Firebase
+import GoogleSignIn
 
-class AuthVC: UIViewController {
+class AuthVC: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -24,13 +24,49 @@ class AuthVC: UIViewController {
         }
     }
     @IBAction func loginWithFacebookDidTap(_ sender: Any) {
+        
     }
 
+    //MARK: - Google SignIn methods
     @IBAction func loginWithGoogleDidTap(_ sender: Any) {
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+    }
+    
+    func sign(_ signIn: GIDSignIn!, present viewController: UIViewController!) {
+        self.present(viewController, animated: true, completion: nil)
+    }
+    
+    func sign(_ signIn: GIDSignIn!, dismiss viewController: UIViewController!) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print("Google SignIn Error : ",error.localizedDescription)
+            return
+        }
+        print("Google didSignIn entered")
+        guard let authentication = user.authentication else { return }
+        let credentials = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        Auth.auth().signIn(with: credentials) { (user, error) in
+            if let error = error {
+                print("Firebase Google Auth Error : ",error.localizedDescription)
+                return
+            }
+            print("User logged in using google account")
+            guard let email = user?.email else { return }
+            guard let provider = user?.providerID else { return }
+            
+            let userData = ["provider": provider, "email": email]
+            DataService.instance.createDBUser(uid: (user?.uid)!, userData: userData)
+            self.dismiss(animated: true, completion: nil)
+        }
     }
     
     @IBAction func loginWithEmailDidTap(_ sender: Any) {
-        
         let loginVC = storyboard?.instantiateViewController(withIdentifier: "LoginVC")
         present(loginVC!, animated: true, completion: nil)
     }
---
+++ Chit Chat/Controller/TBC/TabBarVC.swift
@@ -0,0 +1,24 @@
+//
+//  TabBarVC.swift
+//  Chit Chat
+//
+//  Created by Vinoth Vino on 05/06/18.
+//  Copyright © 2018 CoderEarth. All rights reserved.
+//
+
+import UIKit
+
+class TabBarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
